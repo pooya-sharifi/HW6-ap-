4391e59e097b1a799031041fce6f51e2af9537c1
pooya-sharifi <sharifipooya@gmail.com>
2022-06-04 14:08:49 +0000

    all done and test 4 which i forgot about also done

--- include/q1.h
+++ include/q1.h
@@ -5,20 +5,43 @@
 #include <iostream>
 
 namespace q1 {
-template <typename T, typename Comp>
-T gradient_descent(T a, T b, Comp comp)
+template <typename T, typename Func>
+T gradient_descent(T a, T b, Func func)
 {
     T current_pnt { a };
-    T return_pnt { comp(current_pnt) };
+    T return_pnt { func(current_pnt) };
     std::less<> less;
-    if (less(comp(current_pnt + b), comp(current_pnt)) == 1) {
-        while (less(comp(current_pnt + b), comp(current_pnt))) {
-            return_pnt = comp(current_pnt + b);
+    if (less(func(current_pnt + b), func(current_pnt)) == 1) {
+        while (less(func(current_pnt + b), func(current_pnt))) {
+            return_pnt = func(current_pnt + b);
             current_pnt = current_pnt + b;
         }
     } else {
-        while (less(comp(current_pnt - b), comp(current_pnt))) {
-            return_pnt = comp(current_pnt - b);
+        while (less(func(current_pnt - b), func(current_pnt))) {
+            return_pnt = func(current_pnt - b);
+            current_pnt = current_pnt - b;
+        }
+    }
+
+    std::cout << current_pnt << std::endl;
+    return current_pnt;
+};
+
+template <typename T, typename Func>
+T gradient_descent(T a, T b)
+{
+    Func s;
+    T current_pnt { a };
+    T return_pnt { s(current_pnt) };
+    std::less<> less;
+    if (less(s(current_pnt + b), s(current_pnt)) == 1) {
+        while (less(s(current_pnt + b), s(current_pnt))) {
+            return_pnt = s(current_pnt + b);
+            current_pnt = current_pnt + b;
+        }
+    } else {
+        while (less(s(current_pnt - b), s(current_pnt))) {
+            return_pnt = s(current_pnt - b);
             current_pnt = current_pnt - b;
         }
     }
